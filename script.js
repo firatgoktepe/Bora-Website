7f1ed0a47b6ab849337558b73e7d81f00c958d9e
Firat Göktepe <firat@naxnordic.com>
2021-09-04 17:08:51 +0300

    some js for menu functionalities

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const burger = document.getElementById('burger');
 const ul = document.querySelector('ul.nav');
 
+const x = document.querySelector('button.toggleX')
+
 burger.addEventListener('click', () => {
   ul.classList.toggle('show');
 });
+burger.addEventListener('click', () => {
+  x.classList.toggle('open');
+  burger.classList.toggle('openclose');
+})
+
+x.addEventListener('click', () => {
+  burger.classList.toggle('openclose')
+  x.classList.toggle('open');
+  ul.classList.toggle('show')
+})
